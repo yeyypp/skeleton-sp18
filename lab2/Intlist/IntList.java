c2a207b98180315a650575defdbc66f015cbfae1
shuaiye <18610188361@163.com>
2019-06-18 23:25:53 +0800

    update lab2
    add test if A is null

--- lab2/Intlist/IntList.java
+++ lab2/Intlist/IntList.java
@@ -81,6 +81,9 @@ public class IntList {
      */
 
     public static IntList dcatenate(IntList A, IntList B) {
+        if (A == null) {
+            return B;
+        }
         IntList cur = A;
         while (cur.rest != null) {
             cur = cur.rest;
@@ -94,6 +97,9 @@ public class IntList {
      * * elements of B.  May NOT modify items of A.  Use 'new'.
      */
     public static IntList catenate(IntList A, IntList B) {
+        if (A == null) {
+            return B;
+        }
         IntList tem = new IntList(A.first, null);
         IntList cur = tem;
         while (A.rest != null) {
