d7f90c340a6f1f140f2c420c4de5b16c73d64ccc
shuaiye <18610188361@163.com>
2019-06-18 23:49:09 +0800

    update proj0
    create Planet.class

---
+++ proj0/Planet.java
@@ -0,0 +1,31 @@
+/**
+ * @author ShuaiYe
+ * @date 2019/6/18 23:41
+ */
+public class Planet {
+
+    public double xxPos;
+    public double yyPos;
+    public double xxVel;
+    public double yyVel;
+    public double mass;
+    public String imgFileName;
+
+    public Planet(double xxPos, double yyPos, double xxVel, double yyVel, double mass, String imgFileName) {
+        this.xxPos = xxPos;
+        this.yyPos = yyPos;
+        this.xxVel = xxVel;
+        this.yyVel = yyVel;
+        this.mass = mass;
+        this.imgFileName = imgFileName;
+    }
+
+    public Planet(Planet p) {
+        xxPos = p.xxPos;
+        yyPos = p.yyPos;
+        xxVel = p.xxVel;
+        yyVel = p.yyVel;
+        mass = p.mass;
+        imgFileName = p.imgFileName;
+    }
+}
