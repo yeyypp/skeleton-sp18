2aa08824dd8a47ff88a966f2e7bcb9c97ae7d1a1
shuaiye <18610188361@163.com>
2019-07-31 02:16:47 +0800

    update proj3
    update raster

--- proj3/src/main/java/Rasterer.java
+++ proj3/src/main/java/Rasterer.java
@@ -9,6 +9,13 @@ import java.util.Map;
  */
 public class Rasterer {
 
+    private static final double ULLON = -122.2998046875;
+    private static final double LRLON = -122.2119140625;
+    private static final double ULLAT = 37.892195547244356;
+    private static final double LRLAT = 37.82280243352756;
+    private static final double LENGTH = Math.abs(ULLON) - Math.abs(LRLON);
+    private static final double HEIGHT = ULLAT - LRLAT;
+
     public Rasterer() {
         // YOUR CODE HERE
     }
@@ -42,11 +49,197 @@ public class Rasterer {
      *                    forget to set this to true on success! <br>
      */
     public Map<String, Object> getMapRaster(Map<String, Double> params) {
-        // System.out.println(params);
+
+        boolean query_success = true;
+        int sizeOfGrid = getSizeOfGrid(params);
+
+        double targetUllon = params.get("ullon");
+        double targetUllat = params.get("ullat");
+        double targetLrlon = params.get("lrlon");
+        double targetLrlat = params.get("lrlat");
+
+        String[] ullon = binarySearchUllon(sizeOfGrid, targetUllon);
+        String[] ullat = binarySearchUllat(sizeOfGrid, targetUllat);
+        String[] lrlon = binarySearchLrlon(sizeOfGrid, targetLrlon);
+        String[] lrlat = binarySearchLrlat(sizeOfGrid, targetLrlat);
+
+        int[] upLeft = new int[2];
+        upLeft[0] = Integer.parseInt(ullon[0]);
+        upLeft[1] = Integer.parseInt(ullat[0]);
+
+        int[] lowRight = new int[2];
+        lowRight[0] = Integer.parseInt(lrlon[0]);
+        lowRight[1] = Integer.parseInt(lrlat[0]);
+
+        int Ylength = lowRight[1] - upLeft[1] + 1;
+        int Xlength = lowRight[0] - upLeft[0] + 1;
+
+        int depth = getTargetDepth(params);
+
+        String[][] render_grid = new String[Ylength][Xlength];
+
+        int Xcoor = upLeft[0];
+        int Ycoor = upLeft[1];
+
+        for (int i = 0; i < Ylength; i++) {
+            for (int j = 0; j < Xlength; j++) {
+                render_grid[i][j] = "d" + depth + "_x" + Xcoor + "_y" + Ycoor + ".png";
+                Xcoor++;
+            }
+            Xcoor = upLeft[0];
+            Ycoor++;
+        }
+
+        double raster_ul_lon = Double.valueOf(ullon[1]);
+        double raster_ul_lat = Double.valueOf(ullat[1]);
+        double raster_lr_lon = Double.valueOf(lrlon[1]);
+        double raster_lr_lat = Double.valueOf(lrlat[1]);
+
+
+        System.out.println(params);
         Map<String, Object> results = new HashMap<>();
-        System.out.println("Since you haven't implemented getMapRaster, nothing is displayed in "
-                           + "your browser.");
+        results.put("render_grid", render_grid);
+        results.put("raster_ul_lon", raster_ul_lon);
+        results.put("raster_ul_lat", raster_ul_lat);
+        results.put("raster_lr_lon", raster_lr_lon);
+        results.put("raster_lr_lat", raster_lr_lat);
+        results.put("depth", depth);
+        results.put("query_success", query_success);
         return results;
+    }
+
+    private double getTargetLonDPP(Map<String, Double> params) {
+        double targetLonDPP = (params.get("lrlon") - params.get("ullon")) / params.get("w");
+        return targetLonDPP;
+    }
+
+    public double getLonDPP(double lrlon, double ullon, double width) {
+        return (lrlon - ullon) / width;
+    }
+
+    private int getTargetDepth(Map<String, Double> params) {
+        int targetDepth = 0;
+        double targetLonDPP = getTargetLonDPP(params);
+        for (int i = 0; i <= 7; i++) {
+            double curLonDPP = getLonDPP(LRLON, LRLON - (LENGTH / (Math.sqrt(Math.pow(4, i)))), 256);
+            if (curLonDPP <= targetLonDPP) {
+                targetDepth = i;
+                break;
+            }
+        }
+        if (targetDepth == 0) {
+            targetDepth = 7;
+        }
+        return targetDepth;
+    }
+
+
+
+    private int getSizeOfGrid(Map<String, Double> params) {
+        int depth = getTargetDepth(params);
+        int sizeOfGrid = (int) Math.sqrt(Math.pow(4, depth));
+        return sizeOfGrid;
+    }
+
+    private double[] getUllonLrlon(int sizeOfGrid, int i) {
+        double dis = LENGTH / sizeOfGrid;
+        double ullon = ULLON + dis * i;
+        double lrlon = ULLON + dis * (i + 1);
+        double[] lon = new double[2];
+        lon[0] = ullon;
+        lon[1] = lrlon;
+        return lon;
+    }
+
+    private double[] getUllatLrlat(int sizeOfGrid, int j) {
+        double dis = HEIGHT / sizeOfGrid;
+        double ullat = ULLAT - dis * j;
+        double lrlat = ULLAT - dis * (j + 1);
+        double[] lat = new double[2];
+        lat[0] = ullat;
+        lat[1] = lrlat;
+        return lat;
+    }
+
+    private String[] binarySearchUllon(int sizeOfGrid, double targetUllon) {
+        int left = 0, right = sizeOfGrid - 1;
+
+        double[] lon = null;
+        String[] ans = new String[2];
+        while (left <= right) {
+            int mid = left + (right - left) / 2;
+            lon = getUllonLrlon(sizeOfGrid, mid);
+            if (lon[0] <= targetUllon && lon[1] > targetUllon) {
+                ans[0] = String.valueOf(mid);
+                ans[1] = String.valueOf(lon[0]);
+            }
+            if (lon[0] > targetUllon) {
+                right = mid - 1;
+            } else {
+                left = mid + 1;
+            }
+        }
+        return ans;
+    }
+
+    private String[] binarySearchUllat(int sizeOfGrid, double targetUllat) {
+        int left = 0, right = sizeOfGrid - 1;
+        double[] lat = null;
+        String[] ans = new String[2];
+        while (left <= right) {
+            int mid = left + (right - left) / 2;
+            lat = getUllatLrlat(sizeOfGrid, mid);
+            if (lat[0] >= targetUllat && lat[1] < targetUllat) {
+                ans[0] = String.valueOf(mid);
+                ans[1] = String.valueOf(lat[0]);
+            }
+            if (lat[0] < targetUllat) {
+                right = mid - 1;
+            } else {
+                left = mid + 1;
+            }
+        }
+        return ans;
+    }
+
+    private String[] binarySearchLrlon(int sizeOfGrid, double targetLrlon) {
+        int left = 0, right = sizeOfGrid - 1;
+        double[] lon = null;
+        String[] ans = new String[2];
+        while (left <= right) {
+            int mid = left + (right - left) / 2;
+            lon = getUllonLrlon(sizeOfGrid, mid);
+            if (lon[1] >= targetLrlon && lon[0] < targetLrlon) {
+                ans[0] = String.valueOf(mid);
+                ans[1] = String.valueOf(lon[1]);
+            }
+            if (lon[1] < targetLrlon) {
+                left = mid + 1;
+            } else {
+                right = mid - 1;
+            }
+        }
+        return ans;
+    }
+
+    private String[] binarySearchLrlat(int sizeOfGrid, double targetLrlat) {
+        int left = 0, right = sizeOfGrid - 1;
+        double[] lat = null;
+        String[] ans = new String[2];
+        while (left <= right) {
+            int mid = left + (right - left) / 2;
+            lat = getUllatLrlat(sizeOfGrid, mid);
+            if (lat[1] <= targetLrlat && lat[0] > targetLrlat) {
+                ans[0] = String.valueOf(mid);
+                ans[1] = String.valueOf(lat[1]);
+            }
+            if (lat[1] > targetLrlat) {
+                left = mid + 1;
+            } else {
+                right = mid - 1;
+            }
+        }
+        return ans;
     }
 
 }
